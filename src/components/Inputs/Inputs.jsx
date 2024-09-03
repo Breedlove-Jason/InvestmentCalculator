f707dd9144f15915dc0d08e0b0ea21333945eb87
Jason Breedlove <j@jasonbreedlove.com>
2024-09-02 18:26:03 -0700

    basic investment state for results

--- src/components/Inputs/Inputs.jsx
+++ src/components/Inputs/Inputs.jsx
@@ -1,32 +1,56 @@
 import React, { useState } from "react";
+import {calculateInvestmentResults} from "../../util/investment.js";
 
 function Inputs() {
-  const [initialInvestment, setInitialInvestment] = useState("");
-  const [annualInvestment, setAnnualInvestment] = useState("");
-  const [expectedReturn, setExpectedReturn] = useState("");
-  const [duration, setDuration] = useState("");
+  const [investment, setInvestment] = useState({
+    initialInvestment: '',
+    annualInvestment: '',
+    expectedReturn: '',
+    duration: ''
+  })
+  const [results, setResults] = useState([])
+
+  function handleChange(e){
+    setInvestment({
+        ...investment,
+        [e.target.name]: e.target.value
+    })
+  }
+    function handleSubmit(e) {
+        e.preventDefault()
+        const investmentResults = calculateInvestmentResults({
+            initialInvestment: investment.initialInvestment,
+            annualInvestment: investment.annualInvestment,
+            expectedReturn: investment.expectedReturn,
+            duration: investment.duration
+        })
+      setResults(investmentResults)
+    }
+  {console.log(investment)}
+  console.log(results)
   return (
-    <form id={"user-input"}>
+    <form id={"user-input"} onSubmit={handleSubmit}>
       <div className={"input-group"}>
         <div>
           <label htmlFor={"INITIAL-INVESTMENT"}>{"INITIAL INVESTMENT"}</label>
-          <input type={"number"} name={"INITIAL-INVESTMENT"} />
+          <input type={"number"} name={"initialInvestment"} value={investment.initialInvestment} onChange={handleChange}/>
         </div>
         <div>
           <label htmlFor={"ANNUAL-INVESTMENT"}>{"ANNUAL INVESTMENT"}</label>
-          <input type={"number"} name={"ANNUAL-INVESTMENT"} />
+          <input type={"number"} name={"annualInvestment"} value={investment.annualInvestment}  onChange={handleChange}/>
         </div>
       </div>
       <div className={"input-group"}>
         <div>
           <label htmlFor={"EXPECTED-RETURN"}>{"EXPECTED RETURN"}</label>
-          <input type={"number"} name={"EXPECTED-RETURN"} />
+          <input type={"number"} name={"expectedReturn"} value={investment.expectedReturn} onChange={handleChange}/>
         </div>
         <div>
           <label htmlFor={"DURATION"}>{"DURATION"}</label>
-          <input type={"number"} name={"DURATION"} />
+          <input type={"number"} name={"duration"} value={investment.duration} onChange={handleChange}/>
         </div>
       </div>
+       <button type={"submit"}>{"CALCULATE"}</button>
     </form>
   );
 }
