78b4233bce6407506b1a50e748a8619cc5e43b67
Jason Breedlove <j@jasonbreedlove.com>
2024-09-02 19:52:49 -0700

    work on ResultsTable

--- src/components/Inputs/Inputs.jsx
+++ src/components/Inputs/Inputs.jsx
@@ -1,57 +1,93 @@
-import React, { useState } from "react";
-import {calculateInvestmentResults} from "../../util/investment.js";
+import React, { useState } from 'react';
+import {
+  calculateInvestmentResults,
+  formatter,
+} from '../../util/investment.js';
+import ResultsTable from '../ResultsTable/ResultsTable.jsx';
 
 function Inputs() {
   const [investment, setInvestment] = useState({
     initialInvestment: '',
     annualInvestment: '',
     expectedReturn: '',
-    duration: ''
-  })
-  const [results, setResults] = useState([])
+    duration: '',
+  });
+  const [results, setResults] = useState([]);
 
-  function handleChange(e){
+  function handleChange(e) {
     setInvestment({
-        ...investment,
-        [e.target.name]: e.target.value
-    })
+      ...investment,
+      [e.target.name]: !NaN ? parseFloat(e.target.value) : 0,
+    });
   }
-    function handleSubmit(e) {
-        e.preventDefault()
-        const investmentResults = calculateInvestmentResults({
-            initialInvestment: investment.initialInvestment,
-            annualInvestment: investment.annualInvestment,
-            expectedReturn: investment.expectedReturn,
-            duration: investment.duration
-        })
-      setResults(investmentResults)
-    }
-  {console.log(investment)}
-  console.log(results)
+  function handleSubmit(e) {
+    e.preventDefault();
+    const investmentResults = calculateInvestmentResults({
+      initialInvestment: parseFloat(investment.initialInvestment),
+      annualInvestment: parseFloat(investment.annualInvestment),
+      expectedReturn: parseFloat(investment.expectedReturn),
+      duration: parseFloat(investment.duration),
+    });
+
+    setResults(investmentResults);
+    resetForm();
+  }
+  function resetForm() {
+    setInvestment({
+      initialInvestment: '',
+      annualInvestment: '',
+      expectedReturn: '',
+      duration: '',
+    });
+  }
+
   return (
-    <form id={"user-input"} onSubmit={handleSubmit}>
-      <div className={"input-group"}>
-        <div>
-          <label htmlFor={"INITIAL-INVESTMENT"}>{"INITIAL INVESTMENT"}</label>
-          <input type={"number"} name={"initialInvestment"} value={investment.initialInvestment} onChange={handleChange}/>
+    <>
+      <form id={'user-input'} onSubmit={handleSubmit}>
+        <div className={'input-group'}>
+          <div>
+            <label htmlFor={'INITIAL-INVESTMENT'}>{'INITIAL INVESTMENT'}</label>
+            <input
+              type={'number'}
+              name={'initialInvestment'}
+              value={investment.initialInvestment}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor={'ANNUAL-INVESTMENT'}>{'ANNUAL INVESTMENT'}</label>
+            <input
+              type={'number'}
+              name={'annualInvestment'}
+              value={investment.annualInvestment}
+              onChange={handleChange}
+            />
+          </div>
         </div>
-        <div>
-          <label htmlFor={"ANNUAL-INVESTMENT"}>{"ANNUAL INVESTMENT"}</label>
-          <input type={"number"} name={"annualInvestment"} value={investment.annualInvestment}  onChange={handleChange}/>
+        <div className={'input-group'}>
+          <div>
+            <label htmlFor={'EXPECTED-RETURN'}>{'EXPECTED RETURN'}</label>
+            <input
+              type={'number'}
+              name={'expectedReturn'}
+              value={investment.expectedReturn}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor={'DURATION'}>{'DURATION'}</label>
+            <input
+              type={'number'}
+              name={'duration'}
+              value={investment.duration}
+              onChange={handleChange}
+            />
+          </div>
         </div>
-      </div>
-      <div className={"input-group"}>
-        <div>
-          <label htmlFor={"EXPECTED-RETURN"}>{"EXPECTED RETURN"}</label>
-          <input type={"number"} name={"expectedReturn"} value={investment.expectedReturn} onChange={handleChange}/>
-        </div>
-        <div>
-          <label htmlFor={"DURATION"}>{"DURATION"}</label>
-          <input type={"number"} name={"duration"} value={investment.duration} onChange={handleChange}/>
-        </div>
-      </div>
-       <button type={"submit"}>{"CALCULATE"}</button>
-    </form>
+        <button type={'submit'}>{'CALCULATE'}</button>
+      </form>
+      <ResultsTable data={results} />
+    </>
   );
 }
 
