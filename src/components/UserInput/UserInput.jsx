e5e7eda25171b7689cfa8767836728642463dfc9
Jason Breedlove <j@jasonbreedlove.com>
2024-09-02 21:44:29 -0700

    finish results table

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,22 +1,4 @@
-import React, { useState } from 'react';
-
-function UserInput() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput((prevUserInput) => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: newValue,
-      };
-    });
-  }
-
+function UserInput({ onChange, userInput }) {
   return (
     <section id={'user-input'}>
       <div className={'input-group'}>
@@ -28,7 +10,7 @@ function UserInput() {
             required
             value={userInput.initialInvestment}
             onChange={(event) =>
-              handleChange('initialInvestment', event.target.value)
+              onChange('initialInvestment', event.target.value)
             }
           />
         </p>
@@ -39,7 +21,7 @@ function UserInput() {
             name={'annualInvestment'}
             required
             onChange={(event) =>
-              handleChange('annualInvestment', event.target.value)
+              onChange('annualInvestment', event.target.value)
             }
           />
         </p>
@@ -51,9 +33,7 @@ function UserInput() {
             type={'number'}
             name={'expectedReturn'}
             required
-            onChange={(event) =>
-              handleChange('expectedReturn', event.target.value)
-            }
+            onChange={(event) => onChange('expectedReturn', event.target.value)}
           />
         </p>
         <p>
@@ -62,7 +42,7 @@ function UserInput() {
             type={'number'}
             name={'duration'}
             required
-            onChange={(event) => handleChange('duration', event.target.value)}
+            onChange={(event) => onChange('duration', event.target.value)}
           />
         </p>
       </div>
